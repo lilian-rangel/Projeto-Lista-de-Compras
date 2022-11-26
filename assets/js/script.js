8641e94ca41e67d8d9e0f40412a7163b77c8c8fa
lilian-rangel <lilianrm.nutri@gmail.com>
2022-11-26 10:44:49 -0300

    :construction: grocery-container/querySelector, edit elements/ :lipstick: body

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/* Selecionando os elementos */
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+/* Opções de editar */
+let editElement;
+let editFlag = false;
+let editID = "";
