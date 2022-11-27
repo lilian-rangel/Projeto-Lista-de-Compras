08c795b9bdac31e97fe59533dcdfea2679e60405
lilian-rangel <lilianrm.nutri@gmail.com>
2022-11-27 08:50:55 -0300

    :sparkles: feat: displayAlert

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,33 @@ const clearBtn = document.querySelector('.clear-btn');
 let editElement;
 let editFlag = false;
 let editID = "";
+
+/* Eventos */
+form.addEventListener('submit', addItem);
+
+/* Funções */
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+
+    const id = new Date().getTime().toString();
+    if (value && !editFlag) {
+
+    } else if (value && editFlag) {
+
+    } else{
+        displayAlert("Por favor adicione item", "danger");
+    }
+}
+
+/* Display alerta */
+function displayAlert(text,action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    /* Remover o alerta */
+    setTimeout(function(){
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`);
+    }, 2000);
+}
