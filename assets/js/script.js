91fd4df857c139db22d8804bc17a165218cb184b
lilian-rangel <lilianrm.nutri@gmail.com>
2022-12-01 11:45:20 -0300

    :sparkles: feat: function setupItems | createListItem

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,39 +16,17 @@ let editID = "";
 form.addEventListener('submit', addItem);
 // clear items
 clearBtn.addEventListener('click', clearItems);
+// load items
+window.addEventListener('DOMContentLoaded', setupItems);
 
-/* Funções */
+/* *****Funções***** */
 function addItem(e) {
     e.preventDefault();
     const value = grocery.value;
 
     const id = new Date().getTime().toString();
     if (value && !editFlag) {
-        const element = document.createElement("article");
-        // add class
-        element.classList.add("grocery-item");
-        //add id
-        const attr = document.createAttribute("data-id");
-        attr.value = id;
-        element.setAttributeNode(attr);
-        element.innerHTML = ` <p class="title">${value}</p>
-                                <div class="btn-container">
-                                    <button type="button" class="edit-btn">
-                                        <i class="fas fa-edit"></i>
-                                    </button>
-                                    <button type="button" class="delete-btn">
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-                                </div>`;
-        const deleteBtn = element.querySelector(".delete-btn");
-        const editBtn = element.querySelector(".edit-btn");
-        deleteBtn.addEventListener('click',deleteItem);
-        editBtn.addEventListener('click',editItem);
-
-
-
-        // append child 
-        list.appendChild(element);
+        createListItem(id,value);
         // display alert
         displayAlert("Item adicionado à lista", 'success');
         //show container 
@@ -166,4 +144,39 @@ function getLocalStorage() {
     ? JSON.parse(localStorage.getItem("list"))
     : [];
 }
-// save as strings 
+// Setup items
+function setupItems() {
+    let items = getLocalStorage();
+    if(items.length > 0){
+        items.forEach(function(item){
+            createListItem(item.id, item.value);
+        })
+        container.classList.add("show-container");
+    }
+}
+
+function createListItem(id,value) {
+    const element = document.createElement("article");
+        // add class
+        element.classList.add("grocery-item");
+        //add id
+        const attr = document.createAttribute("data-id");
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = ` <p class="title">${value}</p>
+                                <div class="btn-container">
+                                    <button type="button" class="edit-btn">
+                                        <i class="fas fa-edit"></i>
+                                    </button>
+                                    <button type="button" class="delete-btn">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </div>`;
+        const deleteBtn = element.querySelector(".delete-btn");
+        const editBtn = element.querySelector(".edit-btn");
+        deleteBtn.addEventListener('click',deleteItem);
+        editBtn.addEventListener('click',editItem);
+
+        // append child 
+        list.appendChild(element);
+}
